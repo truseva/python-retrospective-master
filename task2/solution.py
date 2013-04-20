b530a030bae348cb122436e0c34a47186503b40e
truseva <truseva@gmail.com>
2013-04-20 16:40:49 +0300

    remove whitespaces and add blank lines to not contradict with PEP8

--- task2/solution.py
+++ task2/solution.py
@@ -4,30 +4,35 @@ from collections import OrderedDict
 def groupby(func, seq):
     dictionary = {}
     for element in seq:
-        dictionary.setdefault(func(element), []).append(element) 
+        dictionary.setdefault(func(element), []).append(element)
     return OrderedDict(sorted(dictionary.items()))
+
 
 def compose(func1, func2):
     def result(x):
         return func1(func2(x))
     return result
-        
+
+
 def iterate(func):
     result = lambda x: x
     yield result
     while True:
         result = compose(func, result)
         yield result
-                
+
+
 def zip_with(func, *iterables):
     for iterables in zip(*iterables):
         yield func(*iterables)
-                
+
+
 def cache(func, cache_size):
     if cache_size == 0:
         return func
     cache = [(None, None)]*cache_size
-    def func_cached(argument):
+
+	def func_cached(argument):
         for cache_element in cache:
             if cache_element[0] == argument:
                 return cache_element[1]
