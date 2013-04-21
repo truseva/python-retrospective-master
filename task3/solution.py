10657dd47db52fe8d02128a0ff2e08a22162ed3b
truseva <truseva@gmail.com>
2013-04-21 11:17:52 +0300

    remove whitespaces to not contradict with PEP8

--- task3/solution.py
+++ task3/solution.py
@@ -1,6 +1,6 @@
 people = []
- 
- 
+
+
 class Person:
     def __init__(self, name, gender, birth_year, father=None,
                  mother=None):
@@ -10,18 +10,18 @@ class Person:
         self.father = father
         self.mother = mother
         people.append(self)
- 
+
     def __setattr__(self, name, value):
         if (name in ["mother", "father"] and value != None and
             value.birth_year > self.birth_year - 18):
             raise AttributeError("{} is too young!".format(name))
         super().__setattr__(name, value)
- 
+
     def __str__(self):
         return "{}, {}, born in {}".format(self.name,
                                            self.gender,
                                            self.birth_year)
- 
+
     def get_siblings(self, gender='both'):
         siblings = []
         for person in people:
@@ -31,14 +31,14 @@ class Person:
                 ((person.mother == self.mother) or
                 (person.father == person.father))):
                 siblings.append(person)
-        return siblings 
- 
+        return siblings
+
     def get_brothers(self):
         return self.get_siblings('M')
- 
+
     def get_sisters(self):
         return self.get_siblings('F')
- 
+
     def children(self, gender='both'):
         children = []
         for person in people:
@@ -48,13 +48,13 @@ class Person:
                 (self == person.father))):
                 children.append(person)
         return children
-  
+
     def successor(self):
         for child in self.children():
             yield child
             for grandchild in child.successor():
                 yield grandchild
- 
+
     def is_direct_successor(self, other):
         younger, older = ((self, other)
                           if self.birth_year > other.birth_year
